3720942690f5043cbe26421a8071962f33314e4e
Facundo <santillanfacundo43@gmail.com>
2024-04-28 12:33:50 -0300

    Solucionando problema de cors en server.js

--- src/server/server.js
+++ src/server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true,
     withCredentials: true,
   })
