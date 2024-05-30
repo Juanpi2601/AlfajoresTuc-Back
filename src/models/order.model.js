c0bddee63568718916debac2c7d34df47057c148
Facundo <santillanfacundo43@gmail.com>
2024-05-30 00:17:31 -0300

    definiendo update y delet orden en controlador

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,25 +1,66 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  userName: { type: String, required: true }, 
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  userName: { 
+    type: String, 
+    required: true 
+  }, 
   products: [
     {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-      productName: { type: String, required: true },
-      quantity: { type: Number, required: true },
-      price: { type: Number, required: true }
+      productId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Product', 
+        required: true 
+      },
+      productName: { 
+        type: String, 
+        required: true 
+      },
+      quantity: { 
+        type: Number, 
+        required: true 
+      },
+      price: { 
+        type: Number, 
+        required: true 
+      }
     }
   ],
   shippingAddress: {
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    province: { type: String, required: true },
-    postalCode: { type: String, required: true }
+    address: { 
+      type: String, 
+      required: true 
+    },
+    city: { 
+      type: String, 
+      required: true 
+    },
+    province: { 
+      type: String, 
+      required: true 
+    },
+    postalCode: { 
+      type: String, 
+      required: true 
+    }
   },
-  totalPrice: { type: Number, required: true },
-  status: { type: String, default: 'pending' },
-  createdAt: { type: Date, default: Date.now }
+  totalPrice: { 
+    type: Number, 
+    required: true 
+  },
+  status: { 
+    type: String, 
+    default: 'pendiente' 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createOrder, getAllOrders } from '../controllers/order.controller.js';
+import { createOrder, getAllOrders, deleteOrder, updateOrderStatus } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
 router.post('/create', createOrder);
 router.get('/getAll', getAllOrders);
+router.delete('/delete/:id', deleteOrder);
+router.patch('/status/:id', updateOrderStatus);
 
 export default router;
