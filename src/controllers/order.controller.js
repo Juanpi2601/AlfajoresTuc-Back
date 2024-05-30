bf8ecbec5a16cfa7869aea6eadae3b6619f4dc69
Facundo <santillanfacundo43@gmail.com>
2024-05-30 16:34:12 -0300

    agregando controlador de envio de correo mas updateando el estado del mismo

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,10 +1,11 @@
 import Order from '../models/order.model.js';
 import Cart from '../models/cart.model.js';
 import User from '../models/user.model.js'; 
+import nodemailer from 'nodemailer';
 
 export const createOrder = async (req, res) => {
   try {
-    const { userId, shippingAddress } = req.body;
+    const { userId, shippingAddress, email } = req.body; 
 
     const cart = await Cart.findOne({ userId }).populate('products.productId');
 
@@ -20,6 +21,7 @@ export const createOrder = async (req, res) => {
     const order = new Order({
       userId,
       userName: user.name,
+      email: user.email,
       products: cart.products.map(item => ({
         productId: item.productId._id,
         productName: item.productId.nombre,
@@ -42,39 +44,40 @@ export const createOrder = async (req, res) => {
   }
 };
 
+
 export const getAllOrders = async (req, res) => {
-    try {
-        const orders = await Order.find();
+  try {
+    const orders = await Order.find();
 
-        const ordersWithTransformedProducts = orders.map(order => {
-            const productsWithNames = order.products.map(item => {
-                if (item.productId) {
-                    return {
-                        ...item.toObject(),
-                        name: item.productId.nombre,
-                        image: item.productId.imagenUrl
-                    };
-                } else {
-                    console.error('Product ID is null for item:', item);
-                    return {
-                        ...item.toObject(),
-                        name: 'Producto no encontrado',
-                        image: ''
-                    };
-                }
-            });
+    const ordersWithTransformedProducts = orders.map(order => {
+      const productsWithNames = order.products.map(item => {
+        if (item.productId) {
+          return {
+            ...item.toObject(),
+            name: item.productId.nombre,
+            image: item.productId.imagenUrl
+          };
+        } else {
+          console.error('Product ID is null for item:', item);
+          return {
+            ...item.toObject(),
+            name: 'Producto no encontrado',
+            image: ''
+          };
+        }
+      });
 
-            return {
-                ...order.toObject(),
-                products: productsWithNames
-            };
-        });
+      return {
+        ...order.toObject(),
+        products: productsWithNames
+      };
+    });
 
-        res.status(200).json(ordersWithTransformedProducts);
-    } catch (error) {
-        console.error('Error al obtener todas las órdenes:', error);
-        res.status(500).json({ message: 'Ha ocurrido un error al obtener las órdenes' });
-    }
+    res.status(200).json(ordersWithTransformedProducts);
+  } catch (error) {
+    console.error('Error al obtener todas las órdenes:', error);
+    res.status(500).json({ message: 'Ha ocurrido un error al obtener las órdenes' });
+  }
 };
 
 export const deleteOrder = async (req, res) => {
@@ -83,26 +86,32 @@ export const deleteOrder = async (req, res) => {
     const order = await Order.findByIdAndDelete(orderId);
 
     if (!order) {
-        return res.status(404).json({ message: 'Orden no encontrada' });
+      return res.status(404).json({ message: 'Orden no encontrada' });
     }
 
     res.status(200).json({ message: 'Orden eliminada exitosamente' });
-  } 
-  catch (error) {
-      console.error('Error al eliminar la orden:', error);
-      res.status(500).json({ message: 'Error al eliminar la orden' });
+  } catch (error) {
+    console.error('Error al eliminar la orden:', error);
+    res.status(500).json({ message: 'Error al eliminar la orden' });
   }
 };
 
 export const updateOrderStatus = async (req, res) => {
   const { id } = req.params;
-  const { status } = req.body;
+  const { status, trackingNumber } = req.body; 
 
   try {
-    const updatedOrder = await Order.findByIdAndUpdate(id, { status }, { new: true });
+    const updatedOrder = await Order.findByIdAndUpdate(id, { status, trackingNumber }, { new: true });
 
     if (!updatedOrder) {
       return res.status(404).json({ message: 'Pedido no encontrado' });
+    }
+
+    // Envía un correo electrónico de confirmación según el nuevo estado
+    if (status === 'enviado') {
+      await sendShippingConfirmationEmail(updatedOrder);
+    } else if (status === 'completado') {
+      await sendCompletionConfirmationEmail(updatedOrder);
     }
 
     res.status(200).json({ message: 'Estado del pedido actualizado correctamente', order: updatedOrder });
@@ -111,3 +120,63 @@ export const updateOrderStatus = async (req, res) => {
     res.status(500).json({ message: 'Error al actualizar el estado del pedido' });
   }
 };
+
+export const sendShippingConfirmationEmail = async (order) => {
+  try {
+    const { trackingNumber } = order;
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'santillanfacundo43@gmail.com',
+        pass: 'slfzmzpidqymjeux',
+      },
+    });
+
+    const mailOptions = {
+      from: 'santillanfacundo43@gmail.com',
+      to: order.email, 
+      subject: 'Confirmación de Envío',
+      text: `Hola ${order.userName},\n\nTu pedido ha sido enviado. La dirección de envío es: ${order.shippingAddress.address}, ${order.shippingAddress.city}, ${order.shippingAddress.province}, ${order.shippingAddress.postalCode}.\n\nNúmero de seguimiento: ${trackingNumber}\n\nGracias por tu compra.\n\nSaludos,\nTu Aplicación`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log('Correo electrónico de confirmación de envío enviado exitosamente');
+  } catch (error) {
+      console.error('Error al enviar el correo electrónico de confirmación de envío:', error);
+      throw new Error('Error al enviar el correo electrónico de confirmación de envío');
+  }
+};
+
+export const sendCompletionConfirmationEmail = async (order) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'santillanfacundo43@gmail.com',
+        pass: 'slfzmzpidqymjeux',
+      },
+    });
+
+    const mailOptions = {
+      from: 'santillanfacundo43@gmail.com',
+      to: order.email,
+      subject: 'Orden Completada',
+      text: `Hola ${order.userName},\n\nTu pedido ha sido completado. Gracias por tu compra.\n\nSaludos,\nTu Aplicación`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log('Correo electrónico de confirmación de completado enviado exitosamente');
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico de confirmación de completado:', error);
+    throw new Error('Error al enviar el correo electrónico de confirmación de completado');
+  }
+};
+
+
+
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,12 @@
 import express from 'express';
-import { createOrder, getAllOrders, deleteOrder, updateOrderStatus } from '../controllers/order.controller.js';
+import { 
+    createOrder, 
+    getAllOrders, 
+    deleteOrder, 
+    updateOrderStatus,
+    sendShippingConfirmationEmail,
+    sendCompletionConfirmationEmail 
+} from '../controllers/order.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +14,7 @@ router.post('/create', createOrder);
 router.get('/getAll', getAllOrders);
 router.delete('/delete/:id', deleteOrder);
 router.patch('/status/:id', updateOrderStatus);
+router.post('/shipping-email', sendShippingConfirmationEmail);
+router.post('/shipping-email', sendCompletionConfirmationEmail );
 
 export default router;
