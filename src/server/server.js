76ec51ff758fd812401915017f57060a99c66dee
Juanpi2601 <juanpablofortuny89@gmail.com>
2024-06-18 13:11:20 -0300

    server

--- src/server/server.js
+++ src/server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: 'https://main--alfajoresdeltucuman.netlify.app',
+    origin: 'https://funny-churros-c9a23d.netlify.app',
     credentials: true,
     withCredentials: true,
   })
