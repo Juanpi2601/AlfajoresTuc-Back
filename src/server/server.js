16f8ca179f65c591c10fb3213f7c4669d7eaad0e
Juanpi2601 <juanpablofortuny89@gmail.com>
2024-06-24 14:53:41 -0300

    cambiando cors

--- src/server/server.js
+++ src/server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: 'https://alfajoresdeltuc.netlify.app',
+    origin: 'https://alfatuc.netlify.app',
     credentials: true,
     withCredentials: true,
   })
