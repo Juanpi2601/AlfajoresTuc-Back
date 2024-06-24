921013db7e918d3eef12ed3a29f2dda6d1dd56cf
Juanpi2601 <juanpablofortuny89@gmail.com>
2024-06-24 13:57:19 -0300

    cmbindo verifytoken

--- src/validators/verifyToken.js
+++ src/validators/verifyToken.js
@@ -1,16 +1,16 @@
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+export const TOKEN = process.env.TOKEN_SECRET;
 
-export const verifyUserToken = (req, res, next) => {
-  const token = req.header("auth-token");
+export const verifyToken = (req, res) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
 
   if (!token) {
     return res.status(401).json({ message: "Acceso denegado" });
   }
 
   try {
-    const verify = Jwt.verify(token, SECRET); 
-    req.userToken = verify;
-    next();
+    const verified = jwt.verify(token, TOKEN);
+    res.status(200).json(verified);
   } catch (error) {
     res.status(400).json({ message: "Token inválido" });
   }
