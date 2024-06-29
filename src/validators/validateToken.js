61e80d500ad5e3f6e987cd47439317a257383b1b
Facundo <santillanfacundo43@gmail.com>
2024-06-29 00:45:06 -0300

    Modificando user.controllers y validateToken

--- src/validators/validateToken.js
+++ src/validators/validateToken.js
@@ -1,21 +1,22 @@
 import jwt from "jsonwebtoken";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
-const userRequired = (req, res, next) => {
+export const userRequired = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Acceso denegado" });
+    return res.status(401).json({ message: "Acceso denegado, token no proporcionado" });
   }
 
   try {
     const verified = jwt.verify(token, TOKEN_SECRET);
-    req.user = verified;
+    req.user = verified; 
     next();
   } catch (error) {
-    res.status(400).json({ message: "Token inválido" });
+    res.status(401).json({ message: "Token inválido" });
   }
 };
+
 
 export default userRequired;
 
